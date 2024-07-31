d950dfd8417d09ebf1c7f13f31a20943dd02b2c8
Isaac Chan <isaacchan1213@gmail.com>
2024-07-31 05:05:32 -0400

    resume hotfix

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Resume = () => {
     return (
-        <div className='mb-[250px] md:flex flex-col items-center md:mb-[400px]'>
-            <div className='max-w-[450px] md:max-w-[600px] flex flex-col items-center gap-[50px]'>
+        <div className='mb-[250px] items-center md:flex flex-col md:items-center md:mb-[400px]'>
+            <div className='max-w-[450px] m-auto md:max-w-[600px] flex flex-col items-center gap-[50px]'>
                 <div className='text-[30px] md:text-[50px] text-center'>
                     <p>Interested?</p>
                     <p>Check out my resume below.</p>

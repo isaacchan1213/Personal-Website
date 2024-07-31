b3cc7deac0220dcdfe436cc1fe24cae05e7a98c8
Isaac Chan <isaacchan1213@gmail.com>
2024-07-31 02:22:29 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/isaacchan.github.io/",
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+});

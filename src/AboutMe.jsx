211c45b9b333cf20864ebff39c75c5278406cfb0
Isaac Chan <isaacchan1213@gmail.com>
2024-08-01 02:18:07 -0400

    about me description updated

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -8,17 +8,17 @@ const AboutMe = () => {
             </div>
             <div className='w-[100%] flex flex-col gap-[15px] content-start'>
                 <p className='text-[18px] md:text-[20px]'>
-                    Hello! My name is Isaac and I'm a rising sophomore majoring in Computer Science with a minor in Data Science at 
-                    Boston University with a deep interest in software engineering. I started my love for all things tech
-                    ever since high school after reluctantly deciding in my sophomore year to signup for my very first CS class. Though
-                    challenging at the time, it introduced to me how much I enjoyed problem solving and critical thinking, something I have grown
-                    to love about this field. Ever since then, I followed through this passion and never looked back. 
+                    Hello! My name is Isaac and I'm a rising sophomore at Boston University majoring in Computer Science with a minor in Data Science.
+                    I started my love for all things tech ever since high school after reluctantly deciding in my sophomore year to signup for my very 
+                    first CS class. Though challenging at the time, it introduced to me the importance of problem solving and critical thinking, two things in which 
+                    I have grown to love about this field.
                 </p>
                 <p className='text-[18px] md:text-[20px]'>
-                    Fast forward to now, and I've now had so many majors parts of my life revolve around this industry. Whether it be 
-                    having the privilege to land my first role as a software engineer in an ed-tech startup, fostering a community for 
-                    like-minded CS majors through a non-profit, or even deepening my software skills at a fellowship, these are all experiences
-                    I truly love and cherish. 
+                    Fast forwarding to today, I continously immerse myself within this ever growing field. Whether that be through
+                    my internship as a Software Engineer at an ed-tech startup, my vital role as VP of Community at a nonprofit
+                    dedicated in helping CS majors integrate into the tech industry, or even deepening my software skills as a Fellow, 
+                    these are all experiences I truly love and cherish. I am committed to pursing similar opportunities in the future, 
+                    and have no itention on ever looking back.
                 </p>
             </div>
         </div>

92fc5692ad9ccb37a6708608009ce5f1cadedbab
Isaac Chan <isaacchan1213@gmail.com>
2024-08-01 02:48:50 -0400

    about me description changes

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -9,14 +9,14 @@ const AboutMe = () => {
             <div className='w-[100%] flex flex-col gap-[15px] content-start'>
                 <p className='text-[18px] md:text-[20px]'>
                     Hello! My name is Isaac and I'm a rising sophomore at Boston University majoring in Computer Science with a minor in Data Science.
-                    I started my love for all things tech ever since high school after reluctantly deciding in my sophomore year to signup for my very 
+                    I started my love for all things tech ever since high school after reluctantly deciding to signup for my very 
                     first CS class. Though challenging at the time, it introduced to me the importance of problem solving and critical thinking, two things in which 
                     I have grown to love about this field.
                 </p>
                 <p className='text-[18px] md:text-[20px]'>
-                    Fast forwarding to today, I continously immerse myself within this ever growing field. Whether that be through
+                    Fast forwarding to today, I continously immerse myself within this ever growing environment. Whether that be through
                     my internship as a Software Engineer at an ed-tech startup, my vital role as VP of Community at a nonprofit
-                    dedicated in helping CS majors integrate into the tech industry, or even deepening my software skills as a Fellow, 
+                    dedicated in helping CS majors integrate into the tech industry, or even deepening my software engineering skills as a Fellow, 
                     these are all experiences I truly love and cherish. I am committed to pursing similar opportunities in the future, 
                     and have no itention on ever looking back.
                 </p>

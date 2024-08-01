78b9fb55ff2f7c8b0463d486ba4c833703362fc0
Isaac Chan <isaacchan1213@gmail.com>
2024-08-01 01:07:45 -0400

    cover responsive hotfix

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -7,14 +7,14 @@ const AboutMe = () => {
                 <h1 className="text-[25px] md:text-[30px] mb-[20px]">About Me</h1>
             </div>
             <div className='w-[100%] flex flex-col gap-[15px] content-start'>
-                <p className='text-[16px] md:text-[20px]'>
+                <p className='text-[18px] md:text-[20px]'>
                     Hello! My name is Isaac and I'm a rising sophomore majoring in Computer Science with a minor in Data Science at 
                     Boston University with a deep interest in software engineering. I started my love for all things tech
                     ever since high school after reluctantly deciding in my sophomore year to signup for my very first CS class. Though
                     challenging at the time, it introduced to me how much I enjoyed problem solving and critical thinking, something I have grown
                     to love about this field. Ever since then, I followed through this passion and never looked back. 
                 </p>
-                <p className='text-[16px] md:text-[20px]'>
+                <p className='text-[18px] md:text-[20px]'>
                     Fast forward to now, and I've now had so many majors parts of my life revolve around this industry. Whether it be 
                     having the privledge to land my first role as a software engineer in a ed-tech startup, fostering a community for 
                     like-minded CS majors through a non-profit, or even deepening my software skills at a fellowship, these are all experiences
--- src/Cover.jsx
+++ src/Cover.jsx
@@ -3,13 +3,13 @@ import isaacPfp from './assets/isaac-chan-pfp.jpg'
 
 function Cover() {
   return (
-    <div className="flex flex-col relative items-center max-w-[400px] xs:mt-[90px] sm:mt-[0] md:mt-[5rem] md:max-w-5xl m-auto lg:mb-[80px] lg:mt-[90px]">
+    <div className="flex flex-col relative items-center max-w-[400px] md:mt-[8rem] mb-[2.5rem] md:max-w-5xl m-auto lg:mb-[80px] lg:mt-[90px]">
       <div className="flex flex-col-reverse gap-[20px] md:flex-row justify-between items-center">
-        <h3 className="animate-up-down bottom-[8.5rem] left-[13.5rem] text-[20px] sm:bottom-[11.5rem] sm:left-[14rem] sm:text-[30px] md:top-[3.5rem] md:left-[17.5rem] lg:top-[5.5rem] absolute lg:left-[15rem] lg:text-[50px] w-[70px]">🌙</h3>
+        <h3 className="animate-up-down bottom-[9.5rem] left-[13.5rem] text-[20px] sm:bottom-[11.5rem] sm:left-[14rem] sm:text-[30px] md:top-[3.5rem] md:left-[17.5rem] lg:top-[5.5rem] absolute lg:left-[15rem] lg:text-[50px] w-[70px]">🌙</h3>
         <div className='flex flex-col max-w-md gap-[15px] w-full items-center'>
           <h1 className="text-[30px] sm:text-left w-[350px] md:w-[450px]">Hey, I'm <span className="animate-blink-shadow">Isaac.</span></h1>
-          <p className="text-[15px] w-[350px] sm:text-[20px] md:w-[450px] text-left">A passionate and driven individual who is interested in building technical solutions to modern day problems.</p>
-          <p className="text-[15px] w-[350px] sm:text-[20px] md:w-[450px] text-left">Feel free to learn more about me below!</p>
+          <p className="text-[18px] w-[350px] sm:text-[20px] md:w-[450px] text-left">A passionate and driven individual who is interested in building technical solutions to modern day problems.</p>
+          <p className="text-[18px] w-[350px] sm:text-[20px] md:w-[450px] text-left">Feel free to learn more about me below!</p>
         </div>
         <div className='w-[340px] h-[340px] flex-none md:w-[350px] md:h-[350px] lg:w-[450px] lg:h-[450px] md:flex-none'>
           <img className="w-full h-full lg:w-[450px] lg:h-[450px] object-cover rounded-[30%] border-[3px] border-moonYellow" src={isaacPfp}/>

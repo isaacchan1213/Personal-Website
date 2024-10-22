ca7dbb33c6c91ca2fead351083716fe56731247d
Isaac Chan <isaacchan1213@gmail.com>
2024-10-22 18:47:41 -0400

    resume link updated

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
                     <p>Check out my resume below.</p>
                 </div>
                 <div className='button'>
-                    <button className="bg-moonYellow text-darkBlue border-none p-[10px] py-[5px] rounded-[4px] cursor-pointer text-[16px] transform scale-[1.8] hover:border-solid border hover:animate-blink-shadow-button"><a className="no-underline text-black" href='https://drive.google.com/file/d/1t12ItrJFJW_60E8eqlIPNLWCuYiu2FGE/view?usp=sharing' target="_blank" rel="noreferrer noopener">Resume</a></button>
+                    <button className="bg-moonYellow text-darkBlue border-none p-[10px] py-[5px] rounded-[4px] cursor-pointer text-[16px] transform scale-[1.8] hover:border-solid border hover:animate-blink-shadow-button"><a className="no-underline text-black" href='https://drive.google.com/file/d/1BX9SbmxdHkfJselcpQOZNSiejJJ1ayeM/view?usp=sharing' target="_blank" rel="noreferrer noopener">Resume</a></button>
                 </div>
             </div>
         </div>
